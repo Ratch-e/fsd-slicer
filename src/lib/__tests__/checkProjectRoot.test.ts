57e16e1a4fcd010243c0a9fccd0b4d640af5f340
ratch <guninvk@gmail.com>
2025-07-15 17:55:14 +0300

    more tests

---
+++ src/lib/__tests__/checkProjectRoot.test.ts
@@ -0,0 +1,40 @@
+import path from "path";
+import fs from "fs-extra";
+import { runInTempDir } from "../../helpers/tests";
+import { checkProjectRoot } from "../checkProjectRoot";
+
+describe("checkProjectRoot", () => {
+  it("WHEN executed AND package.json exists MUST not exit", async () => {
+    await runInTempDir((cwd) => {
+      fs.writeFileSync(path.join(cwd, "package.json"), "{}");
+      const exitSpy = jest
+        .spyOn(process, "exit")
+        .mockImplementation((() => undefined) as unknown as typeof process.exit);
+      const logSpy = jest.spyOn(console, "log").mockImplementation();
+
+      expect(() => checkProjectRoot()).not.toThrow();
+      expect(exitSpy).not.toHaveBeenCalled();
+      expect(logSpy).not.toHaveBeenCalled();
+
+      exitSpy.mockRestore();
+      logSpy.mockRestore();
+    });
+  });
+
+  it("WHEN executed AND package.json missing MUST exit and log messages", async () => {
+    await runInTempDir(() => {
+      const exitSpy = jest
+        .spyOn(process, "exit")
+        .mockImplementation((() => undefined) as unknown as typeof process.exit);
+      const logSpy = jest.spyOn(console, "log").mockImplementation();
+
+      checkProjectRoot();
+
+      expect(exitSpy).toHaveBeenCalledWith(1);
+      expect(logSpy).toHaveBeenCalled();
+
+      exitSpy.mockRestore();
+      logSpy.mockRestore();
+    });
+  });
+});
